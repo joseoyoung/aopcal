f0d62c1ad59f7f46a0f4cd6ff43af287aa35a856
Seoyoung Jo <josy3465@naver.com>
2018-06-15 10:12:23 +0900

    실행시간 측정하기 3 - 나노시간 측정

--- src/main/java/kr/hs/emirim/joseoyoung/aopcal/main/MainNano.java
+++ src/main/java/kr/hs/emirim/joseoyoung/aopcal/main/MainNano.java
@@ -4,23 +4,23 @@ import kr.hs.emirim.joseoyoung.aopcal.cal.Calculator;
 import kr.hs.emirim.joseoyoung.aopcal.cal.ImpCalculator;
 import kr.hs.emirim.joseoyoung.aopcal.cal.RecCalculator;
 
-public class MainCall {
+public class MainNano {
 
 	public static void main(String[] args) {
-		long num = 10000;
+		long num = 10;
 		
-		long start1 = System.currentTimeMillis();
+		long start1 = System.nanoTime();
 		Calculator cal1 = new ImpCalculator();
 		long f1 = cal1.factorial(num);
-		long end1 = System.currentTimeMillis();
+		long end1 = System.nanoTime();
 		System.out.printf("Imp factorial(%d); 실행시간 = %d", num, (end1-start1));
 		
 		System.out.println();
 		
-		long start2 = System.currentTimeMillis();
+		long start2 = System.nanoTime();
 		Calculator cal2 = new RecCalculator();
 		long f2 = cal2.factorial(num);
-		long end2 = System.currentTimeMillis();
+		long end2 = System.nanoTime();
 		System.out.printf("Rec factorial(%d); 실행시간 = %d", num, (end2-start2));
 		
 		
